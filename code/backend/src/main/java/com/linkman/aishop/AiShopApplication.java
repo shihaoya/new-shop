260566e8131328d568df4b7fd0e13e906937bd6b
danshihao <danshihao@syxysoft.com>
2026-04-28 10:23:10 +0800

    code

---
+++ code/backend/src/main/java/com/linkman/aishop/AiShopApplication.java
@@ -0,0 +1,13 @@
+package com.linkman.aishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiShopApplication.class, args);
+    }
+
+}
---
+++ code/backend/src/test/java/com/linkman/aishop/AiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkman.aishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
